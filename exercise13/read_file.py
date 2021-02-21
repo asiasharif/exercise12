089cfd11063446f31bf8511ac37efda2fc871fb8
Tasnim <Tasnim.b98@gmail.com>
2021-02-21 16:51:29 +0000

    homework done

---
+++ exercise13/read_file.py
@@ -0,0 +1,17 @@
+f = open("pelican.txt", "r")
+content = f.read()
+print(content)
+# print(type(f.read()))
+
+# f_list = f.split(",")
+# f.close()
+# print(f_list)
+
+content_list = content.split(",")
+f.close()
+print(content_list)
+
+print(len(content_list))
+
+for i in content_list:
+    print(i)
