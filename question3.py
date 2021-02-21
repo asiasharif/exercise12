901326d2ee086ab120974dd39b260e9c87101d12
Tasnim <Tasnim.b98@gmail.com>
2021-02-21 14:16:57 +0000

    lottery done

---
+++ question3.py
@@ -0,0 +1,13 @@
+# from random import randint
+#
+# for x in range(6):
+#     lottery = list(range(0,51))
+#
+# print random.randint(0,51)
+#
+# lottery = randint(0,51)
+
+import random
+for x in range(6):
+  list = random.randint(1,51)
+  print(list)
